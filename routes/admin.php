2238b713ca5a647edf16b2087e2e82544fb4c84f
Yudai Nakajima <kituneudon97@gmail.com>
2022-10-22 15:19:50 +0900

    68 その他（route修正、レスポンシブ修正など）

--- routes/admin.php
+++ routes/admin.php
@@ -22,12 +22,9 @@ use App\Http\Controllers\Admin\OwnersController;
 |
 */
 
-Route::get('/', function () {
-  return view('admin.welcome');
-});
-
 Route::resource('owners', OwnersController::class)
-  ->middleware('auth:admin');
+  ->middleware('auth:admin')
+  ->except(['show']);
 
 Route::prefix('expired-owners')->middleware('auth:admin')->group(function () {
   Route::get('index', [OwnersController::class, 'expiredOwnerIndex'])
