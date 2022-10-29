196fbf726db18aaa15000cf51727c36931d76016
Yudai Nakajima <kituneudon97@gmail.com>
2022-10-29 14:39:10 +0900

    71 外部キー制約

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminSeeder::class,
-            OwnerSeeder::class
+            OwnerSeeder::class,
+            ShopSeeder::class
         ]);
     }
 }
