f619ec7599e1ad0e59f28bb1ce4f441ed32f3082
Yudai Nakajima <kituneudon97@gmail.com>
2022-11-01 22:28:39 +0900

    78 カスタムエラーページ

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -12,6 +12,20 @@ class ShopController extends Controller
     public function __construct()
     {
         $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+            $id = $request->route()->parameter('shop');
+            if (!is_null($id)) {
+                $shopsOwnerId = Shop::findOrFail($id)->owner->id;
+                $shopId = (int)$shopsOwnerId;
+                $ownerId = Auth::id();
+                if ($shopId !== $ownerId) {
+                    abort(404);
+                }
+            }
+
+            return $next($request);
+        });
     }
 
     public function index()
