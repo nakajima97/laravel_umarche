c25cd51d85bb3867cac5d9a4e4476afb91d01598
Yudai Nakajima <kituneudon97@gmail.com>
2022-11-15 22:07:56 +0900

    99 Product Index

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -4,9 +4,32 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\SecondaryCategory;
+use App\Models\Owner;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+            $id = $request->route()->parameter('Product');
+            if (!is_null($id)) {
+                $productsOwnerId = Product::findOrFail($id)->owner->id;
+                $productId = (int)$productsOwnerId;
+                if ($productId !== Auth::id()) {
+                    abort(404);
+                }
+            }
+
+            return $next($request);
+        });
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +37,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Owner::findOrFail(Auth::id())->shop->product;
+        return view('owner.products.index', compact('products'));
     }
 
     /**
--- routes/owner.php
+++ routes/owner.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ImageController;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ProductController;
 
 /*
 |--------------------------------------------------------------------------
