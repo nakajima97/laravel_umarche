972086cb498dec3c8c1cbbbc297aa83845417533
Yudai Nakajima <kituneudon97@gmail.com>
2022-11-18 18:44:07 +0900

    105 Micromodal.js その2

--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -28,7 +28,10 @@
                                     <x-input-error :messages="$errors->get('name')" class="mt-2" />
                                 </div>
                             </div>
-                            <x-select-image name="image1" />
+                            <x-select-image :images="$images" name="image1" />
+                            <x-select-image :images="$images" name="image2" />
+                            <x-select-image :images="$images" name="image3" />
+                            <x-select-image :images="$images" name="image4" />
                             <div class="p-2 w-full mt-4 flex justify-around">
                                 <button type="button" onclick="location.href='{{ route('owner.products.index') }}'"
                                     class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
@@ -41,4 +44,23 @@
             </div>
         </div>
     </div>
+    <script>
+        'use strict';
+
+        const images = document.querySelectorAll(".image");
+        images.forEach((image) => {
+            image.addEventListener("click", function(e) {
+                const imageName = e.target.dataset.id.substr(0, 6);
+                const imageId = e.target.dataset.id.replace(imageName + "_", "");
+                const imageFile = e.target.dataset.file;
+                const imagePath = e.target.dataset.path;
+                const modal = e.target.dataset.modal;
+
+                document.getElementById(imageName + "_thumbnail").src =
+                    imagePath + "/" + imageFile;
+                document.getElementById(imageName + "_hidden").value = imageId;
+                MicroModal.close(modal);
+            });
+        });
+    </script>
 </x-app-layout>
