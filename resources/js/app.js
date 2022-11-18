1e009f176b75058118f78ea8c6d89a1e1451ffcc
Yudai Nakajima <kituneudon97@gmail.com>
2022-11-18 18:08:30 +0900

    104 Micromodal.js(画像選択)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
-import './bootstrap';
+import "./bootstrap";
 
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import MicroModal from "micromodal";
+MicroModal.init({
+    disableScroll: true,
+});
--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -28,6 +28,7 @@
                                     <x-input-error :messages="$errors->get('name')" class="mt-2" />
                                 </div>
                             </div>
+                            <x-select-image name="image1" />
                             <div class="p-2 w-full mt-4 flex justify-around">
                                 <button type="button" onclick="location.href='{{ route('owner.products.index') }}'"
                                     class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
