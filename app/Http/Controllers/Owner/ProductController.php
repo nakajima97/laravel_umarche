b51e60f4fa12a24d1bc115f1fb66b987fe5b8a42
Yudai Nakajima <kituneudon97@gmail.com>
2022-11-19 11:47:58 +0900

    #113 Product Update その３

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -128,6 +128,7 @@ class ProductController extends Controller
             ->select('id', 'title', 'filename')
             ->orderBy('updated_at', 'desc')
             ->get();
+        $quantity = Stock::where('product_id', $product->id)->sum('quantity');
 
         $categories = PrimaryCategory::with('secondary')->get();
 
@@ -146,6 +147,53 @@ class ProductController extends Controller
         $request->validate([
             'current_quantity' => 'required|integer'
         ]);
+
+        $product = Product::findOrFail($id);
+        $quantity = Stock::where('product_id', $product->id)->sum('quantity');
+
+        if ($request->current_quantity !== $quantity) {
+            $id = $request->route()->parameter('product');
+            return redirect()->route('owner.products.edit', ['product' => $id])
+                ->with(['message' => '在庫数が変更されています。再度確認してください。', 'status' => 'alert']);
+        } else {
+            try {
+                DB::transaction(function () use ($request, $product) {
+                    $product->name = $request->name;
+                    $product->information = $request->information;
+                    $product->price = $request->price;
+                    $product->sort_order = $request->sort_order;
+                    $product->shop_id = $request->shop_id;
+                    $product->secondary_category_id = $request->category;
+                    $product->image1 = $request->image1;
+                    $product->image2 = $request->image2;
+                    $product->image3 = $request->image3;
+                    $product->image4 = $request->image4;
+                    $product->is_selling = $request->is_selling;
+
+                    $product->save();
+
+                    if ($request->type === '1') {
+                        $newQuantity = $request->quantity;
+                    }
+                    if ($request->type === '2') {
+                        $newQuantity = $request->quantity * -1;
+                    }
+
+                    Stock::create([
+                        'product_id' => $product->id,
+                        'type' => $request->type,
+                        'quantity' => $newQuantity,
+                    ], 2);
+                });
+            } catch (Throwable $e) {
+                Log::error($e);
+                throw $e;
+            }
+
+            return redirect()
+                ->route('owner.products.index')
+                ->with(['message' => '商品を更新しました。', 'status' => 'info']);
+        }
     }
 
     /**
