4baa31902712be0be7a40897d007f23ce8dbcc72
Yudai Nakajima <kituneudon97@gmail.com>
2022-11-19 11:23:07 +0900

    #111 Product Update その1

--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -53,6 +53,7 @@
                                     <input type="number" id="quantity" value="{{ old('quantity') }}" required
                                         name="quantity"
                                         class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                    <span class="font-sm">0〜99の範囲で入力してください</span>
                                     <x-input-error :messages="$errors->get('sort_order')" class="mt-2" />
                                 </div>
                             </div>
