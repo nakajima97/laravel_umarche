10a699a30be0b3448c5c173f4bfaef5ea17398b9
Yudai Nakajima <kituneudon97@gmail.com>
2022-11-21 22:09:48 +0900

    119 ユーザの概要

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,8 @@ class DatabaseSeeder extends Seeder
             ImageSeeder::class,
             CategorySeeder::class,
             // ProductSeeder::class,
-            // StockSeeder::class
+            // StockSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
