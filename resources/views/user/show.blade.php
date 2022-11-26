a1d2c09cf23d5fc97d3feeffeede328793adfa4e
Yudai Nakajima <kituneudon97@gmail.com>
2022-11-26 18:10:29 +0900

    132 Shop情報　その２　Micromodal.js

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -106,10 +106,33 @@
                         @endif
                     </div>
                     <div class="mb-4 text-center">
-                        <button type="button"
+                        <button type="button" data-micromodal-trigger="modal-1" href='javascript:;'
                             class="ml-auto text-white bg-gray-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded">ショップの詳細を見る</button>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="modal micromodal-slide" id="modal-1" aria-hidden="true">
+            <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+                <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
+                    <header class="modal__header">
+                        <h2 class="modal__title" id="modal-1-title" class="text-xl text-gray-700">
+                            {{ $product->shop->name }}
+                        </h2>
+                        <button type="button" class="modal__close" aria-label="Close modal"
+                            data-micromodal-close></button>
+                    </header>
+                    <main class="modal__content" id="modal-1-content">
+                        <p>
+                            {{ $product->shop->information }}
+                        </p>
+                    </main>
+                    <footer class="modal__footer">
+                        <button class="modal__btn" data-micromodal-close type="button"
+                            aria-label="Close this dialog window">閉じる</button>
+                    </footer>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
