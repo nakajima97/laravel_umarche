2f3f1284b4ad3d66b919c396adbf936926bed19c
Yudai Nakajima <kituneudon97@gmail.com>
2022-11-26 19:02:42 +0900

    135 Cart商品を追加

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -74,64 +74,67 @@
                                     <span
                                         class="title-font font-medium text-2xl text-gray-900">{{ number_format($product->price) }}円(税込)</span>
                                 </div>
-                                <div class="flex items-center">
-                                    <span class="mr-3">数量</span>
-                                    <div class="relative">
-                                        <select name="quantity"
-                                            class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                                            @for ($i = 1; $i <= $quantity; $i++)
-                                                <option value="{{ $i }}">{{ $i }}</option>
-                                            @endfor
-                                        </select>
+                                <form action="{{ route('user.cart.add') }}" method="post">
+                                    @csrf
+                                    <div class="flex items-center">
+                                        <span class="mr-3">数量</span>
+                                        <div class="relative">
+                                            <select name="quantity"
+                                                class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+                                                @for ($i = 1; $i <= $quantity; $i++)
+                                                    <option value="{{ $i }}">{{ $i }}</option>
+                                                @endfor
+                                            </select>
+                                        </div>
                                     </div>
-                                </div>
-                                <div>
-                                    <button
-                                        class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">カートに入れる</button>
-                                </div>
+                                    <div>
+                                        <button
+                                            class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">カートに入れる</button>
+                                        <input type="hidden" name="product_id" value="{{ $product->id }}">
+                                </form>
                             </div>
                         </div>
                     </div>
-                    <div class="border-t border-gray-400 my-8">
-                    </div>
-                    <div class="mb-4 text-center">この商品を販売しているショップ</div>
-                    <div class="mb-4 text-center">{{ $product->shop->name }}</div>
-                    <div class="mb-4 text-center">
-                        @if ($product->shop->filename !== null)
-                            <img class="w-40 h-40 rounded-full mx-auto object-cover"
-                                src="{{ asset('storage/shops/' . $product->shop->filename) }}" alt="">
-                        @else
-                            <img src="" alt="">
-                        @endif
-                    </div>
-                    <div class="mb-4 text-center">
-                        <button type="button" data-micromodal-trigger="modal-1" href='javascript:;'
-                            class="ml-auto text-white bg-gray-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded">ショップの詳細を見る</button>
-                    </div>
+                </div>
+                <div class="border-t border-gray-400 my-8">
+                </div>
+                <div class="mb-4 text-center">この商品を販売しているショップ</div>
+                <div class="mb-4 text-center">{{ $product->shop->name }}</div>
+                <div class="mb-4 text-center">
+                    @if ($product->shop->filename !== null)
+                        <img class="w-40 h-40 rounded-full mx-auto object-cover"
+                            src="{{ asset('storage/shops/' . $product->shop->filename) }}" alt="">
+                    @else
+                        <img src="" alt="">
+                    @endif
+                </div>
+                <div class="mb-4 text-center">
+                    <button type="button" data-micromodal-trigger="modal-1" href='javascript:;'
+                        class="ml-auto text-white bg-gray-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded">ショップの詳細を見る</button>
                 </div>
             </div>
         </div>
-        <div class="modal micromodal-slide" id="modal-1" aria-hidden="true">
-            <div class="modal__overlay" tabindex="-1" data-micromodal-close>
-                <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
-                    <header class="modal__header">
-                        <h2 class="modal__title" id="modal-1-title" class="text-xl text-gray-700">
-                            {{ $product->shop->name }}
-                        </h2>
-                        <button type="button" class="modal__close" aria-label="Close modal"
-                            data-micromodal-close></button>
-                    </header>
-                    <main class="modal__content" id="modal-1-content">
-                        <p>
-                            {{ $product->shop->information }}
-                        </p>
-                    </main>
-                    <footer class="modal__footer">
-                        <button class="modal__btn" data-micromodal-close type="button"
-                            aria-label="Close this dialog window">閉じる</button>
-                    </footer>
-                </div>
+    </div>
+    <div class="modal micromodal-slide" id="modal-1" aria-hidden="true">
+        <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+            <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
+                <header class="modal__header">
+                    <h2 class="modal__title" id="modal-1-title" class="text-xl text-gray-700">
+                        {{ $product->shop->name }}
+                    </h2>
+                    <button type="button" class="modal__close" aria-label="Close modal" data-micromodal-close></button>
+                </header>
+                <main class="modal__content" id="modal-1-content">
+                    <p>
+                        {{ $product->shop->information }}
+                    </p>
+                </main>
+                <footer class="modal__footer">
+                    <button class="modal__btn" data-micromodal-close type="button"
+                        aria-label="Close this dialog window">閉じる</button>
+                </footer>
             </div>
         </div>
+    </div>
     </div>
 </x-app-layout>
