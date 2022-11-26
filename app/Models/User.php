778ab2a2f6dc8bb16d412280bc83d55e83ef1e03
Yudai Nakajima <kituneudon97@gmail.com>
2022-11-26 18:48:59 +0900

    134 Cart 多対多

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'carts')
+            ->withPivot(['id', 'quantity']);
+    }
 }
