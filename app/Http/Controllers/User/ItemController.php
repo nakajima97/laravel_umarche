9b7c7455148e58a5bc8e04c89b0e5cb5f6ba5c9f
Yudai Nakajima <kituneudon97@gmail.com>
2022-11-26 15:32:17 +0900

    124 商品一覧のクエリ　その１

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -5,12 +5,24 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
     public function index()
     {
-        $products = Product::all();
+        $stocks = DB::table('t_stocks')
+            ->select('product_id', DB::raw('sum(quantity) as quantity'))
+            ->groupBy('product_id')
+            ->having('quantity', '>', '1');
+
+        $products = DB::table('products')
+            ->joinSub($stocks, 'stock', function ($join) {
+                $join->on('products.id', '=', 'stock.product_id');
+            })
+            ->join('shops', 'products.shop_id', '=', 'shops.id')
+            ->where('shops.is_selling', true)
+            ->get();
 
         return view('user.index', compact('products'));
     }
